9e355fadf02eba8af44f161ed5359a3aabee1cb8
amdreier <113556736+amdreier@users.noreply.github.com>
2023-04-19 17:26:18 -0400

    add LCD display

--- main.c
+++ main.c
@@ -16,6 +16,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include "ST7735.h"
+#include "LCD_GFX.h"
 #include "MIDI.h"
 #include "ADC.h"
 
@@ -24,6 +27,8 @@
 #define CHANNEL 0x01 //Sets the MIDI channel of the cybersynth
 
 extern volatile int ADCArr[6];
+volatile char SelectMode[50];
+char Note[21];
 char val[25];
 
 //Values of the SoftPot and Force Resistive Sensor
@@ -45,6 +50,21 @@ int key_discrete = 0; //Ranges from 0-11 inclusive | 0 -> C maj, 11 -> B maj
 
 
 int quantize_scale_flag = 0; //Flag that keeps track of quantized scale mode or "continuous" mode
+
+char* notes[] = {
+	"C",
+	"C#/Db",
+	"D",
+	"D#/Eb",
+	"E",
+	"F",
+	"F#/Gb",
+	"G",
+	"G#/Fb",
+	"A",
+	"A#/Bb",
+	"B"
+};
 //------------------------------------------------------------------------------------------//
 
 int send_flag = 0;
@@ -55,14 +75,34 @@ void debug(){
 	UART_putstring(val);
 }
 
+void setNote(char* note) {
+	LCD_drawBlock(0, 20, LCD_WIDTH - 1, 26, BLACK);
+	if (NOTE_ON) {
+		char noteStr[10];
+		char octStr[3];
+		int note = curr_note % 12;
+		int noteOctave = (curr_note / 12) - 1;
+		strcpy(noteStr, notes[note]);
+		strcat(noteStr, itoa(noteOctave, octStr, 10));
+		sprintf(Note, "%s", noteStr);
+	} else {
+		sprintf(Note, "X");
+	}
+	
+	LCD_drawString(0, 20, Note, WHITE, BLACK);
+}
+
 /*
 	Initialize function
 */
-void initialize(){
-	UART_init(BAUD_PRESCALER);
-	ADC_Init();
-	
+void initialize() {
 	cli();
+	UART_init(BAUD_PRESCALER);
+	ADC_Init(SelectMode);
+	
+	lcd_init();
+	LCD_setScreen(BLACK);
+	
 	//CONFIGURE TIMEERS/PINS HERE
 	sei();
 	
@@ -113,13 +153,15 @@ int main(void)
 		if(NOTE_ON && !CMD1_SENT){ //If a note is meant to be played and a command has not been sent
 			curr_note = processNote(); //remove temp if not necessary
 			noteOn(CHANNEL, curr_note, 60); //Send command
-			CMD1_SENT = 1; 
+			CMD1_SENT = 1;
+			setNote(Note);
 		} else if (NOTE_ON && CMD1_SENT){
 			int new_note = processNote();
 			if(new_note != curr_note){ //detects slide (i.e. new note being played while force is still being pressed
 				noteOff(CHANNEL, curr_note);
 				noteOn(CHANNEL, new_note, 60);
 				curr_note = new_note;
+				setNote(Note);
 			} else {
 				CMD2_SENT = 0; //CMD2 (noteOff) needs to be sent
 			}
@@ -129,7 +171,9 @@ int main(void)
 			noteOff(CHANNEL, curr_note);
 			CMD1_SENT = 0; //CMD1 (noteOn) now needs to be sent (next note(
 			CMD2_SENT = 1;
+			setNote(Note);
 		}
+		
 		
 		//sendPolyPressure();
 		
